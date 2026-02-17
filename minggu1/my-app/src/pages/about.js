736d5361b4c5f837c6cf728f902f92f8e196b60d
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-02-17 10:59:50 +0700

    tugas praktikum

---
+++ minggu1/my-app/src/pages/about.js
@@ -0,0 +1,14 @@
+import styles from "@/styles/Home.module.css";
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Halaman About</h1>
+        <p>Nama: Agung Fradiansyah</p> <p>NIM: 2341720045</p>
+        <p>Program Studi: D4 Teknik Informatika</p>
+        <a href="/">Kembali ke Home</a>
+      </div>
+    </main>
+  );
+}
