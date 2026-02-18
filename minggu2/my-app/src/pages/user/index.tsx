379e12e8c1e7e25b72b4e4a2d137a6838b8c321a
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-02-18 10:00:53 +0700

    Nested Routing - Buat Folder Setting - test

--- minggu2/my-app/src/pages/user/index.tsx
+++ minggu2/my-app/src/pages/user/index.tsx
@@ -2,7 +2,7 @@ const UserSettingPage = () => {
   return (
     <main style={{ minHeight: "100vh" }}>
       <div style={{ padding: "20px" }}>
-        <h1>App Setting Page</h1>
+        <h1>App Setting Pagee</h1>
       </div>
     </main>
   );
