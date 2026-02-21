1cd01a1902a856adcf75847c8a55f111ed9724c7
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-02-21 19:58:56 +0700

    Membuat Komponen Navbar

--- minggu2/my-app/src/pages/index.tsx
+++ minggu2/my-app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/layouts/navbar";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
       <main style={{ minHeight: "100vh", padding: "20px" }}>
         <div>
+          {/* <Navbar /> */}
           <h1>Praktikum minggu 2</h1>
           <p>Mahasiswa D4 Pengembangan Web</p>
         </div>
---
+++ minggu2/my-app/src/pages/produk/index.tsx
@@ -0,0 +1,9 @@
+const produk = () => {
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <h1>Produk User Page</h1>
+    </div>
+  );
+};
+
+export default produk;
