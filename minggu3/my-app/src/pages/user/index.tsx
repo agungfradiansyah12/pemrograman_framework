34a2f05f2f0a4109e561c21dffe83d2c222ea9bf
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-02-23 15:57:17 +0700

    minggu 3

---
+++ minggu3/my-app/src/pages/user/index.tsx
@@ -0,0 +1,11 @@
+const UserSettingPage = () => {
+  return (
+    <main style={{ minHeight: "100vh" }}>
+      <div style={{ padding: "20px" }}>
+        <h1>User Setting Pagee</h1>
+      </div>
+    </main>
+  );
+};
+
+export default UserSettingPage;
