498a2ee7200effad1419390deed143ec96569f50
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-02-24 20:24:46 +0700

    Validasi Parameter

--- minggu3/my-app/src/pages/shop/[[...slug]].tsx
+++ minggu3/my-app/src/pages/shop/[[...slug]].tsx
@@ -5,13 +5,15 @@ const halamanToko = () => {
   // console.log(Router);
 
   const { query } = useRouter();
+  const slug = query.slug;
 
   return (
     <div>
       <h1>Halaman Toko</h1>
-      <p>Toko: {query.slug && query.slug[0] + "-" + query.slug[1]}</p>
+      {/* <p>Toko: {query.slug && query.slug[0] + "-" + query.slug[1]}</p> */}
       {/* <p>Toko: {`${query.slug && query.slug[0]} - ${query.slug && query.slug[1]}`}</p> */}
       {/* <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p> */}
+      <p>Kategori: {slug ? slug[0] : "Semua Kategori"}</p>
     </div>
   );
 };
