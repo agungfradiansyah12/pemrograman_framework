3cdcc1bc91b6114e3ad4e69577b8deb09abbc8b3
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-03 13:08:04 +0700

    Kombinasi Global CSS + CSS Module

--- minggu4/my-app/src/components/layouts/navbar/index.tsx
+++ minggu4/my-app/src/components/layouts/navbar/index.tsx
@@ -3,7 +3,7 @@ import syles from "./navbar.module.css";
 const Navbar = () => {
   return (
     <div className={syles.navbar}>
-      <h1>Navbar componen</h1>
+      <h1 className="big">Navbar componen</h1>
     </div>
   );
 };
