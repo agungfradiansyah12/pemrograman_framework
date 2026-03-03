56c7d3c554a7a9af74cb4015dd66b0facf9212a3
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-03 15:57:32 +0700

    tugas 3

--- minggu4/my-app/src/pages/views/produk/components/HeroSection.tsx
+++ minggu4/my-app/src/pages/views/produk/components/HeroSection.tsx
@@ -1,7 +1,8 @@
 export default function HeroSection() {
   return (
-    <section>
-      <h1>Produk User Page</h1>
+    <section className="bg-blue-600 text-white py-20 text-center">
+      <h1 className="text-4xl font-bold mb-4">Produk User Page</h1>
+      <p className="text-lg">Selamat datang di halaman produk</p>
     </section>
   );
 }
