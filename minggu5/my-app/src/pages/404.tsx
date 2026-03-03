3637abbf268ffe7b1a416cabaaf27f2094b80183
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-03 22:22:39 +0700

    Tugas 1 (Wajib)

--- minggu5/my-app/src/pages/404.tsx
+++ minggu5/my-app/src/pages/404.tsx
@@ -1,12 +1,20 @@
+import Head from "next/head";
 import styles from "@/styles/404.module.scss";
 
-const Custom404 = () => {
+export default function Custom404() {
   return (
-    <div className={styles.error}>
-      <img src="/page-not-found.png" alt="404" className={styles.error__image} />
-      <h1>404 - Halaman Tidak Ditemukan</h1>
-      <p>Maaf, halaman yang Anda cari tidak ada.</p>
-    </div>
+    <>
+      <Head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+        <meta name="description" content="Halaman tidak tersedia" />
+      </Head>
+
+      <div className={styles.container}>
+        <img src="/page-not-found.png" alt="404" className={styles.image} />
+
+        <h1>404 - Halaman Tidak Ditemukan</h1>
+        <p>Maaf, halaman yang Anda cari tidak tersedia.</p>
+      </div>
+    </>
   );
-};
-export default Custom404;
+}
