854b7c1c812663fbbb28d9a4157f36a331c75e16
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-05 11:56:01 +0700

    Langkah 6 – Install Firebase

---
+++ minggu6/my-app/src/pages/utlis/db/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIHpSGYuZa88S5-NOjG3ma_L0y5-UhPCQ",
+  authDomain: "framework-next-ea3f4.firebaseapp.com",
+  projectId: "framework-next-ea3f4",
+  storageBucket: "framework-next-ea3f4.firebasestorage.app",
+  messagingSenderId: "1013928132765",
+  appId: "1:1013928132765:web:0f025e84a4182a71c45474",
+};
