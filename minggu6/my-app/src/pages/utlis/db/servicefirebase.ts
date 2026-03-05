4f11093b2af044704ed22cedb678fa9d32a3d24d
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-05 15:12:14 +0700

    Langkah 9 – Ambil Data dari Firestore - rename nama dari .js ke .ts

--- minggu6/my-app/src/pages/utlis/db/servicefirebase.ts
+++ minggu6/my-app/src/pages/utlis/db/servicefirebase.ts
@@ -3,7 +3,7 @@ import app from "./firebase";
 
 const db = getFirestore(app);
 
-export async function retrieveProducts(collectionName) {
+export async function retrieveProducts(collectionName: string) {
   const snapshot = await getDocs(collection(db, collectionName));
   const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   return data;
