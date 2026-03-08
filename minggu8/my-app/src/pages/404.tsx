8661418cf3eafb4dd8dc19a5e88c604f36f59f0a
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-08 14:58:30 +0700

    minggu 8

---
+++ minggu8/my-app/src/pages/404.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "@/styles/404.module.scss";
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+        <meta name="description" content="Halaman tidak tersedia" />
+      </Head>
+
+      <div className={styles.container}>
+        <img src="/page-not-found.png" alt="404" className={styles.image} />
+
+        <h1 className={styles.title}>404 - Halaman Tidak Ditemukan</h1>
+        <p>Maaf, halaman yang Anda cari tidak tersedia.</p>
+
+        <Link href="/" className={styles.button}>
+          Kembali ke Home
+        </Link>
+      </div>
+    </>
+  );
+}
