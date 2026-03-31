501c2394e4f93b08383fa775534cf32ca333ea77
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-03-31 19:19:31 +0700

    minggu12 - jobsheet12

---
+++ minggu12/my-app/src/components/layouts/AppShell/index.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import Navbar from "../navbar";
+
+const disableNavbar = ["/auth/login", "/auth/register", "/404"];
+
+type AppShellProps = {
+  children: React.ReactNode;
+};
+
+const AppShell = (props: AppShellProps) => {
+  const { children } = props;
+  const { pathname } = useRouter();
+  return (
+    <main>
+      {/* <Navbar /> */}
+      {!disableNavbar.includes(pathname) && <Navbar />}
+      {children}
+      {/* <footer className="footer">footer</footer> */}
+    </main>
+  );
+};
+
+export default AppShell;
