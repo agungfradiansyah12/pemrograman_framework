ed011d1545afa293ca4ead2d6fef6c61ba8bd26d
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-01 08:23:13 +0700

    tugas jobshett 13

---
+++ minggu13/my-app/src/components/layouts/navbar/index.tsx
@@ -0,0 +1,11 @@
+import syles from "./navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={syles.navbar}>
+      <h1 className="big">Navbar componen</h1>
+    </div>
+  );
+};
+
+export default Navbar;
