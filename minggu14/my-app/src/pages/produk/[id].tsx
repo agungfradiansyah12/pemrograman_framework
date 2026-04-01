b945aca3587063fbec46902f58d39b4c288e9d13
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-01 08:55:38 +0700

    jobsheet 14 - minggu 14

---
+++ minggu14/my-app/src/pages/produk/[id].tsx
@@ -0,0 +1,74 @@
+import fetcher from "@/utils/swr/fetcher";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import DetailProduk from "../../views/DetailProduct";
+import { ProductType } from "@/types/Product.type";
+
+const HalamanProduk = ({ product }: { product: ProductType }) => {
+  {
+    /digunakan client-side rendering/;
+  }
+  // // const Router = useRouter();
+  // // console.log(Router);
+  // const { query } = useRouter();
+  // const { data, error, isLoading } = useSWR(`/api/products/${query.id}`, fetcher);
+  // return (
+  //   <div>
+  //     <DetailProduk products={isLoading ? [] : data.data} />
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <DetailProduk products={product} />
+    </div>
+  );
+};
+
+export default HalamanProduk;
+
+{
+  /digunakan server-side rendering/;
+}
+
+// export async function getServerSideProps({ params }: { params: { id: string } }) {
+//   const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+//   const respone = await res.json();
+//   // console.log("Data produk yang diambil dari API:", respone);
+//   return {
+//     props: {
+//       product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+//     },
+//   };
+// }
+
+{
+  /digunakan static-site generation/;
+}
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response = await res.json();
+
+  const paths = response.data.map((product: ProductType) => ({
+    params: {
+      id: String(product.id), // WAJIB string & nama harus "id"
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params.id}`);
+  const response = await res.json();
+
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
---
+++ minggu14/my-app/src/views/product/index.tsx
@@ -0,0 +1,46 @@
+import styles from "../produk/product.module.scss";
+import Link from "next/link";
+
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+};
+const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+  return (
+    <div className={styles.produk}>
+      <h1 className={styles.produk_title}>Daftar Produk</h1>
+
+      <div className={styles.produk_content}>
+        {products.length > 0 ? (
+          <>
+            {products.map((products: ProductType) => (
+              <Link href={`/produk/${products.id}`} key={products.id} className={styles.produk_content_item}>
+                <div className={styles.produk_content_item_image}>
+                  <img src={products.image} alt={products.name} width={200} />
+                </div>
+
+                <h4 className={styles.produk_content_item_name}>{products.name}</h4>
+
+                <p className={styles.produk_content_item_category}>{products.category}</p>
+
+                <p className={styles.produk_content_item_price}>Rp {products.price.toLocaleString("id-ID")}</p>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <div className={styles.produk_content_skeleton}>
+            <div className={styles.produk_content_skeleton_image}></div>
+            <div className={styles.produk_content_skeleton_name}></div>
+            <div className={styles.produk_content_skeleton_category}></div>
+            <div className={styles.produk_content_skeleton_price}></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TampilanProduk;
