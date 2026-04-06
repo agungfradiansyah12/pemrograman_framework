fe8d8d421425e1ef8be327c5f436469561b6b61f
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-06 12:21:40 +0700

    pertemuan 15

---
+++ minggu15/my-app/src/utils/db/servicefirebase.ts
@@ -0,0 +1,16 @@
+import { getFirestore, collection, getDocs, getDoc, Firestore, doc } from "firebase/firestore";
+import app from "./firebase";
+
+const db = getFirestore(app);
+
+export async function retrieveProducts(collectionName: string) {
+  const snapshot = await getDocs(collection(db, collectionName));
+  const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  return data;
+}
+
+export async function retrieveDataByID(collectionName: string, id: string) {
+  const snapshot = await getDoc(doc(db, collectionName, id));
+  const data = snapshot.data();
+  return data;
+}
