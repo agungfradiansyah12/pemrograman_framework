3b940a9156caa38d6f4c5dd21dd14b6a6b7df925
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-06 20:00:00 +0700

    minggu 16

---
+++ minggu16/my-app/src/utils/db/servicefirebase.ts
@@ -0,0 +1,68 @@
+import { getFirestore, collection, getDocs, Firestore, getDoc, doc, query, addDoc, where } from "firebase/firestore";
+import app from "./firebase";
+import bcrypt from "bcrypt";
+
+const db = getFirestore(app);
+
+export async function retrieveProducts(collectionName: string) {
+  const snapshot = await getDocs(collection(db, collectionName));
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return data;
+}
+
+export async function retrieveDataByID(collectionName: string, id: string) {
+  const snapshot = await getDoc(doc(db, collectionName, id));
+  const data = snapshot.data();
+  return snapshot.data();
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+    role?: string;
+  },
+  callback: Function,
+) {
+  const q = query(collection(db, "users"), where("email", "==", userData.email));
+
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  // console.log("Query result:", data);
+
+  if (data.length > 0) {
+    // user sudah ada → TOLAK
+    callback({
+      status: "error",
+      message: "User already exists",
+    });
+  } else {
+    // user belum ada → daftar
+    userData.password = await bcrypt.hash(userData.password, 10);
+
+    userData.role = "user";
+    userData.role = "member";
+
+    await addDoc(collection(db, "users"), userData)
+      .then(() => {
+        callback({
+          status: "success",
+          message: "User registered successfully",
+        });
+      })
+      .catch((error) => {
+        callback({
+          status: "error",
+          message: error.message,
+        });
+      });
+  }
+}
