6933e56cd56b15a8c0601fefbb93d202fa45cd02
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-08 13:32:14 +0700

    praktikum 5

--- minggu19/my-app/jest.config.mjs
+++ minggu19/my-app/jest.config.mjs
@@ -1,5 +1,6 @@
 import { TestEnvironment } from "jest-environment-jsdom";
 import nextJest from "next/jest.js";
+
 const createJestConfig = nextJest({
   dir: "./",
 });
@@ -7,6 +8,8 @@ const createJestConfig = nextJest({
 const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  collectConverage: true,
+  collectCoverageFrom: ["**/*.{ts,tsx}", "**/*.d.ts", "!**/node_modules/**", "!**/.next/**", "!**/coverage/**", "!**/jest.config.mjs", "!**/next.config.mjs", "!**/types/**", "!**/views/**", "!**/page/api**"],
 };
 
 export default createJestConfig(config);
