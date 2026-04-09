5d6486493e6f3a4d184bdfb1788326a89bf8d89d
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-09 14:36:47 +0700

    praktikum 7

---
+++ minggu19/my-app/src/__test__/product.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import TampilanProduk from "@/pages/produk";
+
+// Mock router
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/produk",
+      pathname: "/produk",
+      query: {},
+      asPath: "/produk",
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      isReady: true,
+    };
+  },
+}));
+
+describe("Product Page", () => {
+  it("renders product page correctly", () => {
+    const { container } = render(<TampilanProduk />);
+
+    expect(screen.getByTestId("title").textContent).toBe("Product Page");
+    expect(container).toMatchSnapshot();
+  });
+});
