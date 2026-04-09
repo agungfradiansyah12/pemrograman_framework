05aa8959c51113449248e9f50ad823958257486a
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2026-04-09 14:25:02 +0700

    Praktikum 6

--- minggu19/my-app/src/pages/about/index.tsx
+++ minggu19/my-app/src/pages/about/index.tsx
@@ -1,11 +1,21 @@
 // import styles from "@/styles/Home.module.css";
 
-export default function About() {
+// export default function About() {
+//   return (
+//     <main style={{ minHeight: "100vh", padding: "20px" }}>
+//       <div>
+//         <h1>Ini Halaman About</h1>
+//       </div>
+//     </main>
+//   );
+// }
+
+const AboutPage = () => {
   return (
-    <main style={{ minHeight: "100vh", padding: "20px" }}>
-      <div>
-        <h1>Ini Halaman About</h1>
-      </div>
-    </main>
+    <div>
+      <h1 data-testid="title">About Page</h1>
+    </div>
   );
-}
+};
+
+export default AboutPage;
